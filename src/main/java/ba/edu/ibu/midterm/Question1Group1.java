f5d384998b05e05f7c50b2222ab73845cda9c911
Adnan Miljkovic <195312+adnanmiljkovic@users.noreply.github.com>
2023-11-08 15:27:20 +0100

    Midterm questions

---
+++ src/main/java/ba/edu/ibu/midterm/Question1Group1.java
@@ -0,0 +1,62 @@
+package ba.edu.ibu.midterm;
+
+import java.util.HashMap;
+
+
+/*
+
+Create a Java interface called Enrollable with an abstract method enroll().
+Implement this interface in a class Student who enrolls in a course. The enroll() in Student should print “Enrolled in a course”.
+
+Now, create another interface called Payment with an abstract method makePayment(String date, double amount).
+Implement the Payment interface in the above Student class, allowing students to make payments.
+There can be multiple payments for a single day.
+Create a method called getPayment(String date) that returns the total amount on the give day.
+
+ */
+interface Enrollable {
+    void enroll();
+}
+
+interface Payment {
+    void makePayment(String date, double amount);
+}
+
+class Student implements Enrollable, Payment {
+    private HashMap<String, Double> payments;
+
+    public Student() {
+        payments = new HashMap<>();
+    }
+
+    @Override
+    public void enroll() {
+        System.out.println("Enrolled in a course");
+    }
+
+    @Override
+    public void makePayment(String date, double amount) {
+        if (payments.containsKey(date)) {
+            double currentAmount = payments.get(date);
+            payments.put(date, currentAmount + amount);
+        } else {
+            payments.put(date, amount);
+        }
+    }
+
+    public double getPayment(String date) {
+        return payments.get(date);
+    }
+}
+
+class Question1Group1 {
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.enroll();
+        student.makePayment("20", 100.0);
+        student.makePayment("20", 200.0);
+        System.out.println(student.getPayment("20"));
+    }
+
+}
---
+++ src/main/java/ba/edu/ibu/midterm/Question1Group2.java
@@ -0,0 +1,62 @@
+package ba.edu.ibu.midterm;
+
+import java.util.HashMap;
+
+
+/*
+
+Create a Java interface called Course with an abstract method teach().
+Implement this interface in a class Teacher who teaches in a course. The teach() in Teacher should print “Teaching in a course”.
+
+Now, create another interface called Salary with an abstract method receivePayment(String date, double amount).
+Implement the Salary interface in the above Teacher class, allowing teachers to receive payment.
+There can be multiple payments for a single day.
+Create a method called getAllPayments(String date) that returns the total amount on the give day.
+
+ */
+interface Course {
+    void teach();
+}
+
+interface Salary {
+    void receivePayment(String date, double amount);
+}
+
+class Teacher implements Course, Salary {
+    private HashMap<String, Double> payments;
+
+    public Teacher() {
+        payments = new HashMap<>();
+    }
+
+    @Override
+    public void teach() {
+        System.out.println("Teaching in a course");
+    }
+
+    @Override
+    public void receivePayment(String date, double amount) {
+        if (payments.containsKey(date)) {
+            double currentAmount = payments.get(date);
+            payments.put(date, currentAmount + amount);
+        } else {
+            payments.put(date, amount);
+        }
+    }
+
+    public double getAllPayments(String date) {
+        return payments.get(date);
+    }
+}
+
+class Question1Group2 {
+
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        teacher.teach();
+        teacher.receivePayment("20", 100.0);
+        teacher.receivePayment("20", 200.0);
+        System.out.println(teacher.getAllPayments("20"));
+    }
+
+}
