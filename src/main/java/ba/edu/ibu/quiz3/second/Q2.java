11583ea693fd89a72211a1856ed5a7c8672e1de5
ibecir <becir@bioptimizers.com>
2023-11-10 10:04:50 +0100

    The third quiz questions

---
+++ src/main/java/ba/edu/ibu/quiz3/second/Q2.java
@@ -0,0 +1,42 @@
+package ba.edu.ibu.quiz3.second;
+
+interface Readable {
+    public String getContent();
+}
+
+class SMS implements Readable {
+
+    private String message;
+
+    public SMS(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String getContent() {
+        return this.message;
+    }
+}
+
+class Book implements Readable {
+    private String page;
+
+    public Book(String page) {
+        this.page = page;
+    }
+
+    @Override
+    public String getContent() {
+        return this.page;
+    }
+}
+
+class MainC2 {
+    public static void main(String[] args) {
+        SMS sms = new SMS("My first message");
+        Book book = new Book("The page");
+
+        System.out.println(sms.getContent());
+        System.out.println(book.getContent());
+    }
+}
