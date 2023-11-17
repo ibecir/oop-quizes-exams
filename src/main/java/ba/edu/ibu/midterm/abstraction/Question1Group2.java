dcf7ca6b3d06f09123e135d451ba6fb57f46ac93
Adnan Miljkovic <195312+adnanmiljkovic@users.noreply.github.com>
2023-11-17 13:01:18 +0100

    update abstraction 2nd group question

--- src/main/java/ba/edu/ibu/midterm/abstraction/Question1Group2.java
+++ src/main/java/ba/edu/ibu/midterm/abstraction/Question1Group2.java
@@ -3,12 +3,13 @@ package ba.edu.ibu.midterm.abstraction;
 
 /*
 
-Create an abstract class called PointsProcess with an abstract method addPoints(int points).
+Create an abstract class called PointsProcess with two abstract methods addPoints(int points) and isValidPoints(int minPoints, int maxPoints, int points).
 
 Extend this abstract class in a class called StudentGPA, add a variable total of type int and studentName of type String.
 Create a construction that will accept the studentName as a parameter.
 
 Implement the addPoints() method in the StudentGPA class, adding the new points to the total and printing "Value of X processed.".
+Implement the isValidPoints() method in the StudentGPA class, checking if the points are valid based on the min and max provided. For valid points return true, otherwise false
 
 Create a method called currentPoints() that returns the current total.
 
@@ -16,6 +17,8 @@ Create a method called currentPoints() that returns the current total.
  */
 abstract class PointsProcess {
     abstract void addPoints(int points);
+
+    abstract boolean isValidPoints(int minPoints, int maxPoints, int points);
 }
 
 class StudentGPA extends PointsProcess {
@@ -37,16 +40,40 @@ class StudentGPA extends PointsProcess {
         System.out.println("Value of " + points + " processed.");
     }
 
-
+    @Override
+    boolean isValidPoints(int minPoints, int maxPoints, int points) {
+        if (points >= minPoints && points <= maxPoints) {
+            return true;
+        }
+        return false;
+    }
 }
 
 public class Question1Group2 {
     public static void main(String[] args) {
+
+        //test ONE
         StudentGPA studentGpa = new StudentGPA("adnan");
         studentGpa.addPoints(5);
         studentGpa.addPoints(10);
         studentGpa.addPoints(5);
-
         System.out.println(studentGpa.currentPoints());
+
+
+        //test TWO
+        studentGpa = new StudentGPA("adnan");
+        if (studentGpa.isValidPoints(1,100, 50)) {
+            System.out.println("Valid points");
+        }
+
+        //test THREE
+        studentGpa = new StudentGPA("adnan");
+        if (!studentGpa.isValidPoints(1,100, 110)) {
+            System.out.println("Invalid points");
+        }
+
+
+
+
     }
 }
